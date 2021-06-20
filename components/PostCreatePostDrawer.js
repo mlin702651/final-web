d6c80c9fed4a1caea848cdd1185eeb3adcb97bbe
yuudachi31 <yuudachi31@gmail.com>
2021-06-21 01:21:33 +0800

    drawer

--- components/PostCreatePostDrawer.js
+++ components/PostCreatePostDrawer.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import { useContext, useEffect, useState,useRef } from "react";
-import { Layout, Menu, Breadcrumb, Drawer, Button, Select  ,Input} from 'antd';
+import { useContext, useEffect, useState, useRef } from "react";
+import { Layout, Menu, Breadcrumb, Drawer, Button, Select, Input } from 'antd';
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 import PostsHeader from "../components/PostsHeader.js"
 import PostsContent from "../components/PostsContent.js"
@@ -11,7 +11,7 @@ const { Header, Content, Footer, Sider } = Layout;
 import { getProducts } from "../api";
 import moment from 'moment'
 import postsJson from "../json/posts.json"
-import {postNewArticle} from "../actions"
+import { postNewArticle } from "../actions"
 import { StoreContext } from "../store"
 import { set } from 'js-cookie';
 
@@ -37,7 +37,7 @@ const PostCreatePostDrawer = () => {
 
 
   }, []);
-  const { state: { userSignin:{
+  const { state: { userSignin: {
     loading,
     userInfo,
     remember,
@@ -50,15 +50,16 @@ const PostCreatePostDrawer = () => {
   //
   const [pauseLogin, setPauseLogin] = useState(false)
   const [taglist, setTaglist] = useState([])
-  const[tagOnChange,setTagOnChange]=useState("")
-  const[fileExist,setFileExist]=useState(false)
-  const[posttext,setPosttext]=useState("")
+  const [tagOnChange, setTagOnChange] = useState("")
+  const [fileExist, setFileExist] = useState(false)
+  const [posttext, setPosttext] = useState("")
   //
-  const[postRN,setPostRN]=useState("")
+  const [postRN, setPostRN] = useState("")
   //
-  const[postArea,setPostArea]=useState("北部")
+  const [postArea, setPostArea] = useState("北部")
   //
-  const[postCity,setPostCity]=useState("台北")
+  const [postCity, setPostCity] = useState("台北")
+  const [selectedArea, setSelectedArea] = useState("北部")
   //
 
   // console.log(postsJson)
@@ -78,22 +79,22 @@ const PostCreatePostDrawer = () => {
   };
   const onTagchange = e => {
     console.log('Change:', e.target.value);
-    var tag =e.target.value
+    var tag = e.target.value
     setTagOnChange(tag)
-    
+
   };
-  function addtag(){
+  function addtag() {
     console.log(tagOnChange)
-     const tagExist=taglist.find((x) => x === tagOnChange);
-  if(tagExist){
-    console.log("已經有了")
-  }else if(tagOnChange){
- setTaglist([...taglist,tagOnChange])
- setTagOnChange(null)
-  }
-  else{
-    console.log("蝦")
-  }
+    const tagExist = taglist.find((x) => x === tagOnChange);
+    if (tagExist) {
+      console.log("已經有了")
+    } else if (tagOnChange) {
+      setTaglist([...taglist, tagOnChange])
+      setTagOnChange(null)
+    }
+    else {
+      console.log("蝦")
+    }
   }
   const removeTag = tag => {
     // e.target.getAttribute("name")
@@ -102,25 +103,27 @@ const PostCreatePostDrawer = () => {
     // var tagindex = taglist.indexOf(tag);
     // console.log(taglist)
     // taglist.splice(tagindex, 1)
-      
+
     setTaglist(taglist.filter(item => item !== tag));
 
-  // setTaglist(taglist)
-console.log("a")
+    // setTaglist(taglist)
+    console.log("a")
   };
-  
+
 
 
 
   function handleAreaChange(value) {
     console.log(`selected ${value}`);
+    setPostCity(null)
     setPostArea(value)
+    setSelectedArea(value)
   }
   function handleCityChange(value) {
     console.log(`selected ${value}`);
     setPostCity(value)
   }
- 
+
   function handleFileSelect(evt) {
     var file = evt.target.files[0];
     var reader = new FileReader();
@@ -154,34 +157,41 @@ console.log("a")
       }
     }
   }
-function Postsubmit(){
- 
-  if(userInfo){
-    let time = moment().format('YYYY-MM-DD HH:mm:ss')
-let postInfo={
-  "user":userInfo.displayName,
-  "uid":userInfo.uid,
-  "date":time,
-  "area":postArea,
-  "city":postCity,
-  "image":imagedata,
-  "restaurant":postRN,
-  "text":posttext,
-  "tags":taglist,
-  "comments":[]
-}
-
-
-setVisible(false)
-console.log(userInfo)
-   postNewArticle(dispatch,postInfo)
-   setTaglist([])
-   setImagedata("")
-   
-  }else{
-    console.log("no way")
+  function drawerOpen() {
+    if (userInfo) {
+      setVisible(true)
+    } else {
+      alert("請先登入或註冊")
+    }
   }
-}
+  function Postsubmit() {
+
+    if (userInfo) {
+      let time = moment().format('YYYY-MM-DD HH:mm:ss')
+      let postInfo = {
+        "user": userInfo.displayName,
+        "uid": userInfo.uid,
+        "date": time,
+        "area": postArea,
+        "city": postCity,
+        "image": imagedata,
+        "restaurant": postRN,
+        "text": posttext,
+        "tags": taglist,
+        "comments": []
+      }
+
+
+      setVisible(false)
+      console.log(userInfo)
+      postNewArticle(dispatch, postInfo)
+      setTaglist([])
+      setImagedata("")
+
+    } else {
+      console.log("no way")
+    }
+  }
 
 
   return (
@@ -197,86 +207,100 @@ console.log(userInfo)
       style={{ position: 'fixed', zIndex: 90 }}
     >
       {/* <div className="create-post-drawer-cont1"> */}
-      <div className="create-post-drawer-open-btn" onClick={() => { setVisible(!visible) }}>
+      <div className="create-post-drawer-open-btn" onClick={drawerOpen}>
 
       </div>
-      {visible?(
-      <div className="create-post-drawer-elm-cont">
-        <div className="post-imgUploader-cont">
-          <div className="imgUploader-window">
-          {fileExist?(<Example data={imagedata} />):(null) }
-          </div>
-          <div className="imgUploader-file">
-            <input onChange={inputChange} className="imgUploader-file2" type="file" accept="image/*" capture="camera" id="64input"></input>
-            <div className="imgUploader-file-text">選擇圖片</div>
-
-          </div>
-        </div>
-        <div className="post-infoUploader-cont">
-          <div className="area-and-city-cont">
-            <div className="area-selector">
-              <Select defaultValue="地區" style={{ width: 120 }} onChange={handleAreaChange}>
-                <Option value="北部">北部</Option>
-                <Option value="中部">中部</Option>
-                <Option value="南部" > 南部</Option>
-                <Option value="西部">西部</Option>
-              </Select>
+      {visible ? (
+        <div className="create-post-drawer-elm-cont">
+          <div className="post-imgUploader-cont all-h">
+            <div className="imgUploader-window">
+              {fileExist ? (<Example data={imagedata} />) : (null)}
             </div>
-            <div className="city-selector">
-              <Select defaultValue="縣市" style={{ width: 120 }} onChange={handleCityChange}>
-                <Option value="基隆">基隆</Option>
-                <Option value="台北">台北</Option>
-                <Option value="新北">新北</Option>
-                <Option value="桃園">桃園</Option>
-                <Option value="新竹">新竹</Option>
-                <Option value="苗栗">苗栗</Option>
-                <Option value="台中">台中</Option>
-                <Option value="彰化">彰化</Option>
-                <Option value="南投">南投</Option>
-                <Option value="雲林">雲林</Option>
-                <Option value="嘉義">嘉義</Option>
-                <Option value="台南">台南</Option>
-                <Option value="高雄">高雄</Option>
-                <Option value="屏東">屏東</Option>
-                <Option value="台東">台東</Option>
-                <Option value="花蓮">花蓮</Option>
-                <Option value="宜蘭">宜蘭</Option>
-              </Select>
+            <div className="imgUploader-file">
+              <input onChange={inputChange} className="imgUploader-file2" type="file" accept="image/*" capture="camera" id="64input"></input>
+              <div className="imgUploader-file-text">選擇圖片</div>
+
             </div>
           </div>
-          <div className="post-restaurant-name">
-          <Input placeholder="輸入拉麵店" onChange={onRNChange} />
-          </div>
-          <div className="post-text-input">
+          <div className="post-infoUploader-cont all-h">
+            <div className="area-and-city-cont">
+              <div className="area-selector">
+                <Select defaultValue="北部" style={{ fontSize: 22 }} onChange={handleAreaChange}>
+                  <Option style={{ fontSize: 22, marginBottom: 5 }} value="北部">北部</Option>
+                  <Option style={{ fontSize: 22, marginBottom: 5 }} value="中部">中部</Option>
+                  <Option style={{ fontSize: 22, marginBottom: 5 }} value="南部" > 南部</Option>
+                  <Option style={{ fontSize: 22, marginBottom: 5 }} value="東部">東部</Option>
+                </Select>
+              </div>
+              <div className="city-selector">
+                <Select defaultValue="城市" style={{ fontSize: 22 }} onChange={handleCityChange}>
+                  {selectedArea === "北部" ?
+                    (<><Option style={{ fontSize: 22, marginBottom: 5 }} value="基隆">基隆</Option>
+                      <Option style={{ fontSize: 22, marginBottom: 5 }} value="台北">台北</Option>
+                      <Option style={{ fontSize: 22, marginBottom: 5 }} value="新北">新北</Option>
+                      <Option style={{ fontSize: 22, marginBottom: 5 }} value="桃園">桃園</Option>
+                      <Option style={{ fontSize: 22, marginBottom: 5 }} value="新竹">新竹</Option>
+                    </>) :
+                    selectedArea === "中部" ?
+                      (<><Option style={{ fontSize: 22, marginBottom: 5 }} value="苗栗">苗栗</Option>
+                        <Option style={{ fontSize: 22, marginBottom: 5 }} value="台中">台中</Option>
+                        <Option style={{ fontSize: 22, marginBottom: 5 }} value="彰化">彰化</Option>
+                        <Option style={{ fontSize: 22, marginBottom: 5 }} value="南投">南投</Option>
+                        <Option style={{ fontSize: 22, marginBottom: 5 }} value="雲林">雲林</Option>
+                      </>) :
+                      selectedArea === "南部" ?
+                        (<> <Option style={{ fontSize: 22, marginBottom: 5 }} value="嘉義">嘉義</Option>
+                          <Option style={{ fontSize: 22, marginBottom: 5 }} value="台南">台南</Option>
+                          <Option style={{ fontSize: 22, marginBottom: 5 }} value="高雄">高雄</Option>
+                          <Option style={{ fontSize: 22, marginBottom: 5 }} value="屏東">屏東</Option>
+                        </>) :
+                        selectedArea === "東部" ?
+                          (<><Option style={{ fontSize: 22, marginBottom: 5 }} value="台東">台東</Option>
+                            <Option style={{ fontSize: 22, marginBottom: 5 }} value="花蓮">花蓮</Option>
+                            <Option style={{ fontSize: 22, marginBottom: 5 }} value="宜蘭">宜蘭</Option>
+                          </>) : (null)
+                  }
 
-          <TextArea onChange={onTextChange} />
-     
+
+
+
+
+                </Select>
+              </div>
+            </div>
+            <div className="c-post-restaurant-name">
+              <Input placeholder="輸入拉麵店" onChange={onRNChange} />
+            </div>
+            <div className="post-text-input">
+
+              <TextArea onChange={onTextChange} />
+
+            </div>
+          </div>
+          <div className="posts-submit-btn-cont all-h">
+            <div className="addTag">
+              <div className="taginput">
+                <Input placeholder="標籤" onChange={onTagchange} value={tagOnChange} />
+                <div className="addTag-btn" onClick={addtag}>+</div>
+              </div>
+              <div className="addTag-tags">
+                {
+                  taglist.map(tag => (
+                    <div key={tag} className="addTag-eachtag">
+                      <div ref={btnRef} className="tag-remove-btn" name={tag} onClick={() => {
+                        removeTag(tag);
+                      }}>X</div>
+                      <div className="addtag-tag-text">{tag}</div>
+
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
+            <div className="posts-submit-btn" onClick={Postsubmit}>發布</div>
           </div>
         </div>
-        <div className="posts-submit-btn-cont">
-          <div className="addTag">
-          <div className="taginput">
-          <Input placeholder="標籤" onChange={onTagchange} value={tagOnChange}/>
-          <div className="addTag-btn" onClick={addtag}>+</div>
-          </div>
-          <div className="addTag-tags">
-{
-  taglist.map(tag=>(
-    <div key={tag}className="addTag-eachtag">
-      <div ref={btnRef}className="tag-remove-btn" name={tag} onClick={()=>{
-        removeTag(tag);
-      }}>X</div>
-<div className="addtag-tag-text">{tag}</div>
-
-    </div>
-  ))
-}
-          </div>
-          </div>
-          <div className="posts-submit-btn" onClick={Postsubmit}>發布</div>
-        </div>
-      </div>
-      ):(null)}
+      ) : (null)}
     </Drawer>
 
 
